61eb590488d2287f39fdff0e026e95b226db40f2
Jet Hu <774372605@qq.com>
2018-10-02 12:16:12 +0800

    Add Boiling prediction project

---
+++ CoolReact/boiling-prediction/build/precache-manifest.001e00173599c3e8e6cf36388627db0a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b0da6288189c97d13b7b",
+    "url": "/static/js/main.b0da6288.chunk.js"
+  },
+  {
+    "revision": "1355d8189a912ecb20a6",
+    "url": "/static/js/1.1355d818.chunk.js"
+  },
+  {
+    "revision": "b0da6288189c97d13b7b",
+    "url": "/static/css/main.f1c949a3.chunk.css"
+  },
+  {
+    "revision": "1e218ccf322cafa19925979d7829fef7",
+    "url": "/index.html"
+  }
+];
